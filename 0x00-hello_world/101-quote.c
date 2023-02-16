ae5bf4529862fff679b160335e1b6b8c4edc8275
Tillymolete <Mahlatsemolete@gmail.com>
2023-02-16 08:42:47 -0800

    UNIX

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <unistd.h>
 /**
  * main - C program printing a line followed by a new line to standard error
@@ -5,6 +6,6 @@
  */
 int main(void)
 {
-write("and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", stdout);
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
