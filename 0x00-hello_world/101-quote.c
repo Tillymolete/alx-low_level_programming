3ef68607b8f6fde3f5c9c9bc63434acb478f411e
Tillymolete <Mahlatsemolete@gmail.com>
2023-02-16 08:38:51 -0800

    UNIX

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+/**
+ * main - C program printing a line followed by a new line to standard error
+ * Return: 1;
+ */
+int main(void)
+{
+write("and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
