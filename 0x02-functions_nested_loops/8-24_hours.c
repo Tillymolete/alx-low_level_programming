692e66e48b2dce210ed8ade5ac1ab4778943aa5c
Tillymolete <Mahlatsemolete@gmail.com>
2023-02-21 13:00:23 -0800

    8-24_hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * jack_bauer - prints every minute of the day of Jack Bauer 
- * Return: ...
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ * * Return: ...
  */
 
 void jack_bauer(void)
