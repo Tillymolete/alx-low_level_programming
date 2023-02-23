d60f4223388aa248cd89aee9a5880416c503a638
Tillymolete <Mahlatsemolete@gmail.com>
2023-02-23 08:14:28 -0800

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,7 +2,7 @@
 
 /**
  * _isupper - checks for uppercase character
- * @c: input
+ * @c: entry input
  * Return: 1 if c is uppercase else 0
  */
 
@@ -12,5 +12,6 @@ int _isupper(int c)
 	{
 	return (1);
 	}
+	else 
 	return (0);
 }
