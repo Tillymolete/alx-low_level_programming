4c2ad5a667d7074ecbdc0fb70ee7854e0505a0ec
Tillymolete <Mahlatsemolete@gmail.com>
2023-02-23 10:31:02 -0800

    4-print_most_numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints the numbers 0-9 except 2 and 4
+ * do not print 2 and 4
+ * Return: the numbers 0 up to 9
+ */
+
+void print_most_numbers(void)
+{
+	int x;
+
+	for (; x <= 9; x++)
+	{
+		if (x == 2 || x == 4)
+		{
+		continue;
+		}
+		else
+		{
+		_putchar(x + '0');
+		}
+	}
+	_putchar('\n');
+}
