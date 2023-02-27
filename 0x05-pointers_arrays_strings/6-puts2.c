559f04622d92c330674c5cf572fd9dcc9310c31c
Tillymolete <Mahlatsemolete@gmail.com>
2023-02-27 11:49:15 -0800

    6puts

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * @str:  the string to be printed
+ * Return: Nothing
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+	
+	while (str[i] != '\0')
+	{
+		putchar(str[i]);
+		i += 2;
+	}
+	putchar('\n');
+}
