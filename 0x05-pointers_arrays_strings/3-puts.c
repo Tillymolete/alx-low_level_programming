66d0f4f53efbf6d90889f912c35deb03ec7c0207
Tillymolete <Mahlatsemolete@gmail.com>
2023-02-27 08:10:32 -0800

    3puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ * @str: this is the string character
+ * Return: nothing
+ */
+
+void _puts(char *str)
+{
+	char *str;
+
+	str = "I don't fear computers";
+	_puts("str\n", str);
+}
