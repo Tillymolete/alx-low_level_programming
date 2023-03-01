12f960ee98e108f67b2896d3184a19a77b06a5ed
Tillymolete <Mahlatsemolete@gmail.com>
2023-03-01 13:06:41 -0800

    leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * leet - This will encodes a string into 1337
+ * @str: the pointer of the string
+ * Return: a pointer to the encoded string
+ */
+
+char *leet(char *str)
+{
+	char *ptr = str;
+	char *leet_chars = "aAeEoOtTlL";
+	char *leet_nums = "4433007711";
+
+	for (; *ptr != '\0'; ptr++)
+	{
+		for (int i = 0; i < 10; i++)
+		{
+			if (*ptr == leet_chars[i])
+			{
+				*ptr = leet_nums[i];
+				break;
+			}
+		}
+	}
+return (str);
+}
