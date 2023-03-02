40c633f679051af991338c7a88894ae56accc2dc
Tillymolete <Mahlatsemolete@gmail.com>
2023-03-02 00:13:26 -0800

    leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,18 +10,18 @@
 
 char *leet(char *str)
 {
+	char *leet_chars = "aAeEoOtTlL";
+	char *lett_nums = "4433007711";
 	int i = 0;
 	int j = 0;
-	char *leet_chars = "aAeEoOtTlL";
-	char *leet_nums = "4433007711";
 
-	for (i = 0; str[i] ; i++)
+	for (i = 0; str[i]; i++)
 	{
 		for (j = 0; leet_chars[j]; j++)
 		{
-			if (str[i] == leet_char[j])
+			if (str[i] == leet_chars[j])
 			{
-				str[i] = leet_nums[j \ 2];
+				str[i] == leet_nums[j / 2];
 				break;
 			}
 		}
