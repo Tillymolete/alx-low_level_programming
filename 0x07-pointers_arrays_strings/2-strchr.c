f26c54d57209d7f529445a60cff519768cc4c68c
Tillymolete <Mahlatsemolete@gmail.com>
2023-03-06 06:59:57 -0800

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -6,22 +6,23 @@
  * @c: the first character
  * Return: NULL or a pointer
  */
-
 char *_strchr(char *s, char c)
 {
-        int a = 0;
+	int a = 0, b;
 
-        while (s[a])
-        {
-                a++;
-        }
-        for (b = 0; b <= a; b++)
-        {
-                if (c == s[b])
-                {
-                        s += b;
-                        return (s);
-                }
-        }
-        return ('\0');
+	while (s[a])
+	{
+		a++;
+	}
+
+	for (b = 0; b <= a; b++)
+	{
+		if (c == s[b])
+		{
+			s += b;
+			return (s);
+		}
+	}
+
+	return ('\0');
 }
