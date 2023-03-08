4efec747dde2753667802d3adea882dc02de936d
Tillymolete <Mahlatsemolete@gmail.com>
2023-03-08 03:21:57 -0800

    sqrtrecursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: integer
+ * Return: the integer
+ */
+
+int sqrt_helper(int n, int i);
+int _sqrt_recursion(int n)
+{
+	if (n < 1)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (sqrt_helper(n, 1));
+	}
+}
+
+int sqrt_helper(int n, int i)
+{
+	if (i * i > n)
+	{
+		return (-1);
+	}
+	else if (i * i == n)
+	{
+		return(i);
+	}
+	else
+	{
+		return (sqrt_helper(n, i + 1));
+	}
+}
