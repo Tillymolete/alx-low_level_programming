8901e7c826b8164b8c2cb3d2930dd656531c0ff1
Tillymolete <Mahlatsemolete@gmail.com>
2023-03-08 05:07:45 -0800

    ispalindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * is_palindrome - returns 1 if a string is a palindrome and 0 if not
+ * @s: input string
+ * Return: the integer
+ */
+
+int is_palindrome(char *s)
+{
+	int len =strlen(s);
+	int i = 0;
+	int j = len - 1;
+
+	while (i < j)
+	{
+		if (s[i] != s[j])
+		{
+			return (0);
+		}
+		i++;
+		j--;
+	}
+	return (10);
+}
