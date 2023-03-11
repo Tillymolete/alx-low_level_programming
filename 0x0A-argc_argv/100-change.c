3c86a58b44800757415916d41e288a11cd4696fd
Tillymolete <Mahlatsemolete@gmail.com>
2023-03-10 16:40:15 -0800

    change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -8,36 +8,45 @@
  * Return: 1 for number not 1 and 0 otherwise
  */
 
-intmain(int argc, char *argv[]) {
-if (argc != 2) {
-printf("Error\n");
-return 1;
-}
+int main(int argc, char *argv[])
+{
+	int cents, coins = 0;
 
-int cents = atoi(argv[1]);
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
 
-if (cents < 0) {
-printf("0\n");
-return 0;
-}
+	cents = atoi(argv[1]);
 
-int num_coins = 0;
+	while (cents > 0)
+	{
+		coins++;
+		if ((cents - 25) >= 0)
+		{
+			cents -= 25;
+			continue;
+		}
+		if ((cents - 10) >= 0)
+		{
+			cents -= 10;
+			continue;
+		}
+		if ((cents - 5) >= 0)
+		{
+			cents -= 5;
+			continue;
+		}
+		if ((cents - 2) >= 0)
+		{
+			cents -= 2;
+			continue;
+		}
+		cents--;
+	}
 
-num_coins += cents / 25;
-cents %= 25;
+	printf("%d\n", coins);
 
-num_coins += cents / 10;
-cents %= 10;
-
-num_coins += cents / 5;
-cents %= 5;
-
-num_coins += cents / 2;
-cents %= 2;
-
-num_coins += cents;
-
-printf("%d\n", num_coins);
-
-return 0;
+	return (0);
 }
