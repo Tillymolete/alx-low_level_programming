ca36edf7e3b65f7081cf8ee4a18c0366b24c837c
Tillymolete <Mahlatsemolete@gmail.com>
2023-03-13 03:03:08 -0700

    stringdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * _strdup - returns a pointer allocated space in memory.
@@ -10,17 +11,18 @@
 
 char *_strdup(char *str)
 {
+	char *dup_str;
+
 	if (str == NULL)
 	{
 		return (NULL);
 	}
-	*dup_str = malloc(strlen(str + 1);
-				if (dup_str == NULL)
-				{
-				return (NULL);
-				}
-			strcpy(sup_str, str);
+	dup_str = malloc(strlen(str + 1));
+	if (dup_str == NULL)
+	{
+		return (NULL);
+	}
+			strcpy(dup_str, str);
 
 			return (dup_str);
-			}
-
+}
