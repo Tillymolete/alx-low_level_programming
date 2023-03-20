465fc35aca28e64362a3608f3c1dd8b7e9b05866
Tillymolete <Mahlatsemolete@gmail.com>
2023-03-20 08:06:30 -0700

    newdog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -16,29 +16,65 @@ typedef struct dog
 		char *name;
 		float age;
 		char *owner;
-	}dog_t;
+	};
+int _strlen(char *str);
+char *strcopy(char *dest, char *src);
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_dog_ptr = malloc(sizeof(dog_t));
+	dog_t *new_dog_ptr;
+
+	new_dog_ptr = malloc(sizeof(dog_t));
 
 	if (new_dog_ptr == NULL)
 	{
 		return (NULL);
 	}
-	new_dog_ptr->name = malloc(strlen(name) + 1);
-	new_dog_ptr->owner = malloc(strlen(owner) + 1);
 
-	if (new_dog_ptr->name == NULL || new_dog_ptr->owner == NULL)
+	if (name == NULL || owner == NULL)
+		return (NULL);
+
+	new_dog_ptr->name = malloc (sizeof(char) * (_strlen(name) + 1));
+
+	if (new_dog_ptr->name == NULL)
 	{
-		free(new_dog_ptr->name);
-		free(new_dog_ptr->owner);
 		free(new_dog_ptr);
-
 		return (NULL);
 	}
-	strcpy(new_dog_ptr->name, name);
-	strcpy(new_dog_ptr->owner, owner);
-
+	strcopy(new_dog_ptr->name, name);
 	new_dog_ptr->age = age;
+	_strcopy(new_dog_ptr->owner, owner);
+
 	return (new_dog_ptr);
+
+/**
+ * _strlen - the string length
+ * @str: the measured string
+ * Return: the length of the measured string
+ */
+
+_strlen(char *str)
+{
+	int len = 0;
+	
+	while (*str++)
+		len++;
+	return (len);
+}
+
+/**
+ * _strcopy - copies the string from source
+ * @src: the source string
+ * @dest: the destination of the buffer sorting string
+ * Return: the pointer destination
+ */
+
+char *_strcopy(char *dest, char *src)
+{
+	int i = 0;
+	
+	while ((dest[i] = src[i]) != '\0')
+		i++;
+	return (dest);
+}
 }
