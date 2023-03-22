4b3fdbbb7f3418f4d9409e8db5da96af4d9ddd3b
Tillymolete <Mahlatsemolete@gmail.com>
2023-03-22 03:52:46 -0700

    mainopcodes

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the opcodes of its own main function.
+ * @argc: argument count.
+ * @argv: argument vector.
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	char *opcodes = char (*) main;
+	int i, num_bytes;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	num_bytes = atoi(argv[1]);
+
+	if (num_bytes <= 0)
+	{
+		printf("Error\n");
+		return (2);
+	}
+	for (i = 0; i < num_bytes; i++)
+	{
+		printf("%02x", opcodes[i]);
+	}
+	printf("\n");
+
+	return (0);
+}
