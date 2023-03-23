9d71dd0839c8259b7224f78d85fd64a537f626c2
Tillymolete <Mahlatsemolete@gmail.com>
2023-03-23 13:20:30 -0700

    printall

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -8,61 +8,60 @@
 
 void print_all(const char * const format, ...)
 {
-va_list args;
-char *str;
-unsigned i = 0, j;
-char c;
-float f;
-const char t_arg[] = "cifs";
-
-va_start(args, format);
-
-while (format && format[i])
-{
-j = 0;
-while (t_arg[j])
-{
-if (format[i] == t_arg[j])
-{
-if (j > 0)
-{
-printf(", ");
-}
-break;
-}
-j++;
-}
-switch (format[i])
-{
-case 'c':
-c = (char) va_arg(args, int);
-printf("%c", c);
-break;
-case 'i':
-i = va_arg(args, int);
-printf("%d", i);
-break;
-case 'f':
-f = (float) va_arg(args, double);
-printf("%f", f);
-break;
-case 's':
-str = va_arg(args, char *);
-
-if (str == NULL)
-{
-printf("(nil)");
-}
-else
-{
-printf("%s", str);
-}
-break;
-default:
-break;
-}
-i++;
-}
-va_end(args);
-printf("\n");
+	va_list args;
+	char *str;
+	unsigned int i = 0, j;
+	char c;
+	float f;
+	const char t_arg[] = "cifs";
+	
+	va_start(args, format);
+	
+	while (format && format[i])
+	{
+		j = 0;
+		while (t_arg[j])
+		{
+			if (format[i] == t_arg[j])
+			{
+				if (j > 0)
+				{
+					printf(", ");
+				}
+				break;
+			}
+			j++;
+		}
+		switch (format[i])
+		{
+			case 'c':
+				c = (char) va_arg(args, int);
+				printf("%c", c);
+				break;
+			case 'i':
+				i = va_arg(args, int);
+				printf("%d", i);
+				break;
+			case 'f':
+				f = (float) va_arg(args, double);
+				printf("%f", f);
+				break;
+			case 's':
+				str = va_arg(args, char *);
+				if (str == NULL)
+				{
+					printf("(nil)");
+				}
+				else
+				{
+					printf("%s", str);
+				}
+				break;
+			default:
+				break;
+		}
+		i++;
+	}
+	va_end(args);
+	printf("\n");
 }
