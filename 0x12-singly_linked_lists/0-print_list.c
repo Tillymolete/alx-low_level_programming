63874fdeddfd805c7e7a16a52da73a70d9b1f28c
Tillymolete <mahlatsemolete@gmail.com>
2023-03-30 10:47:00 -0700

    printlists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -5,9 +5,9 @@ size_t print_list(const list_t *h)
 {
 	size_t count = 0;
 
-	while (h != NULL)
+	while (h)
 	{
-		if (h->str != NULL)
+		if (h->str == NULL)
 			printf("[%d %s\n", h->len, h->str);
 		else
 			printf("[0] (nil)\n");
