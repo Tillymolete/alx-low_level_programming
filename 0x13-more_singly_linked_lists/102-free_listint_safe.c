383311ae3f1533993a603fa40fa9537fe6fc5b0f
Tillymolete <mahlatsemolete@gmail.com>
2023-04-04 11:14:01 -0700

    freelistintsafe

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,6 +1,50 @@
 #include "lists.h"
 #include <stdlib.h>
 
+size_t looped_listint_count(listint_t *head);
+size_t free_listint_safe(listint_t **h);
+/**
+ * looped_listint_count - count the number of nodes in the looped list
+ * @head: a pointer to the head of the listint_t
+ * @Return: 0 if list is not looped, otherwise the number of nodes
+ */
+
+size_t looped_listint_count(listint_t *head)
+{
+	listint_t *tortoise, *hare;
+	size_t size = 1;
+
+	if (head == NULL || head->next == NULL)
+		return (0);
+
+	tortoise = head->next;
+	hare = (head->next)->next;
+
+	while (hare)
+	{
+		if (tortoise == hare)
+		{
+			tortoise = head;
+			while (tortoise != hare)
+			{
+				size++;
+				tortoise = tortoise->next;
+				hare = hare->next;
+			}
+			tortoise = tortoise->next;
+			while (tortoise != hare)
+			{
+				size++;
+				tortoise = tortoise->next;
+			}
+			return (size);
+		}
+		tortoise = tortoise->next;
+		hare = (hare->next)->next;
+	}
+	return (0);
+}
+
 /**
  * free_listint_safe - frees a listint_t list
  * @h: pointer to a pointer to the head of the list
@@ -10,31 +54,29 @@
 size_t free_listint_safe(listint_t **h)
 {
 	listint_t *current, *tmp;
-	size_t size = 0;
-	hash_table_t *ht = NULL;
+	size_t size, index;
 
-	ht = create_hash_table(1024);
+	size = looped_listint_count(*h);
 
-	if (ht == NULL)
-		exit(98);
-
-	current = *h;
-	while (current != NULL)
+	if (size == 0)
 	{
-		if (hash_table_t(ht, current, ""))
+		for(; h != NULL && *h != NULL; size++)
 		{
-			tmp = current;
-			current = current->next;
-			free(tmp);
-			size++;
-		}
-		else
-		{
-			breake;
+			tmp = (*h)->next;
+			free(*h);
+			*h = tmp;
 		}
 	}
-	free_hash_table(ht);
-	*h = NULL;
-
+	else
+	{
+		for (index = 0; index < size; index++)
+		{
+			tmp = (*h)->next;
+			free(*h);
+			*h = tmp;
+		}
+		*h = NULL;
+	}
+	h = NULL;
 	return (size);
 }
