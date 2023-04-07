ad8ee359df02df322cb0fa8ea8357d7ea8d6a193
Tillymolete <mahlatsemolete@gmail.com>
2023-04-07 09:33:13 -0700

    readtextfile

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * read_textfile - reads a text file, prints it to POSIX standard output
+ * @filename: a string
+ * @letters: the maximum number of characters
+ * Return: number of printed letters, at failure print 0
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buffer;
+	ssize_t bytes_read = 0;
+	ssize_t bytes_written = 0;
+	FILE *fp = NULL;
+
+	if (filename == NULL)
+		return (0);
+
+	fp = fopen(filename, "r");
+	 
+	if (fp == NULL)
+		return (0);
+
+	buffer = malloc(letters + 1);
+
+	if (buffer == NULL)
+		return (0);
+
+	bytes_read = fread(buffer, 1, letters, fp);
+
+	if (bytes_read <= 0)
+		return (0);
+
+	buffer[bytes_read] = '\0';
+	bytes_written = fwrite(buffer, 1, bytes_read, stdout);
+
+	 if (bytes_written != bytes_read)
+		 return (0);
+
+	 fclose(fp);
+	 free(buffer);
+
+	 return (bytes_written);
+}
