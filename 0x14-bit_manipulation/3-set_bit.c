bfa9c3af3fd13c0d3f1fdce18563b41a9d93dcc0
Tillymolete <mahlatsemolete@gmail.com>
2023-07-07 11:07:27 -0700

    commit aall updates

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -2,21 +2,3 @@
 #include "main.h"
 
 /**
- * set_bit - sets the value of a bit to 1
- * @n: pointer of unsigned long int
- * @index: index of the bit
- * Return: 1 success otherwise -1
- */
-
-int set_bit(unsigned long int *n, unsigned int index)
-{
-	unsigned int mask;
-
-	if (index > 63)
-		retun (-1);
-
-	mask = 1 << index;
-	*n = (*n | mask);
-
-	return (1);
-}
