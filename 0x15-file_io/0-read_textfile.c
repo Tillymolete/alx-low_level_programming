b2d376ca3176f5758ca20273c71b95c4928ec748
Tillymolete <mahlatsemolete@gmail.com>
2023-07-10 10:16:51 -0700

    reads and printsletters

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -22,7 +22,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (fp == NULL)
 		return (0);
 
-	buffer = malloc(letter + 1);
+	buffer = malloc(letters + 1);
 	if (buffer == NULL)
 		return (0);
 
