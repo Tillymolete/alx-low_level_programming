11df84b672e42124065e45792207ca9a6a389237
Tillymolete <mahlatsemolete@gmail.com>
2023-07-10 10:45:33 -0700

    textappendedattheend

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, len;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+	
+	if (fd < 0)
+	       return (-1);
+
+	if (text_content != NULL)
+	{
+		len = strlen(text_content);
+
+		if (write(fd, text_content, len) != len)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+	close(fd);
+	return (1);
+}
