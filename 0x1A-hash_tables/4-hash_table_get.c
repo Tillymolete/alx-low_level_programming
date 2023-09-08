1fb50123431530e1a29690ee883d9e2f1cff3a7a
Tillymolete <Mahlatsemolete@gmail.com>
2023-09-08 17:19:44 +0000

    hashtables

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,33 @@
+#include <string.h>
+#include <stdlib.h>
+#include "hash_table.h"
+
+/**
+ * hash_table_get - retrieves a value associated with a key
+ * @ht: hash table to  search in.
+ * @key: The key to get the value of.
+ *
+ * Return: If the key cannot be matched - NULL.
+ *         Otherwise - the value associated with key in ht.
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *current;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
+
+	index = key_index((const unsigned char *)key, ht->size);
+
+	current = ht->array[index];
+
+	while (current != NULL)
+	{
+		if (strcmp(current->key, key) == 0);
+			return (current->value);
+		current = current->next;
+	}
+	return (NULL);
+}
